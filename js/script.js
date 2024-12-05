717376c5ff708fa44ea183b1c8b6e894967367fd
carlosvillaar <carlosvillar004@gmail.com>
2024-12-05 12:14:35 +0100

    fix: correct syntax error

--- js/script.js
+++ js/script.js
@@ -7,20 +7,20 @@ axios.get(api_url)
   dataApi = response.data;
   for (let i = 0; i < dataApi.length; i++){
     const {albumId, id, title, url, thumbnailUrl} = dataApi[i];
-    containerCard.innerHTML += `<div class="custom-card text-bg-light position-relative mx-4">
+    printCards(url, title)
+  }
+})
+
+function printCards(par1, par2){
+  containerCard.innerHTML += `<div class="custom-card text-bg-light position-relative mx-4">
         <div class="pin">
           <img src="./assets_day1/img/pin.svg" alt="pin">
         </div>
         <div class="card-img d-flex flex-column justify-content-center align-items-center">
-          <img src="${url}" alt="">
+          <img src="${par1}" alt="placeholder img">
         </div>
         <div class="card-text">
-          <p>${title}</p>
+          <p>${par2}</p>
         </div>
       </div>`
-  }
-  
-})
-
-
-
+}
