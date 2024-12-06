5cfa3fe0c7812b9428ab3572f297762a653be22b
carlosvillaar <carlosvillar004@gmail.com>
2024-12-06 12:44:51 +0100

    feat: add functions: hide elements, show elements, print image on the overlay

--- js/script.js
+++ js/script.js
@@ -1,5 +1,9 @@
 const api_url = 'https://jsonplaceholder.typicode.com/photos?_limit=6';
 const containerCard = document.querySelector('.container-card');
+const overlay = document.getElementById('overlay');
+const overlayButton = document.querySelector('.close');
+const overlayImg = document.getElementById('overlay-img');
+const clickableImage = document.querySelector('.clickabel-img')
 
 let dataApi;
 axios.get(api_url)
@@ -8,16 +12,16 @@ axios.get(api_url)
   for (let i = 0; i < dataApi.length; i++){
     const {albumId, id, title, url, thumbnailUrl} = dataApi[i];
     printCards(url, capitalizeWords(title));
+    addEventListener('click', () =>{
+      printImg(url)
+      show(overlay)
+    })
   }
 })
 
 .catch((err)=> {
   console.log(`Errore, riprova piu tardi`, err);
-  
 });
-
-
-
 
 function printCards(par1, par2){
   containerCard.innerHTML += `<div class="custom-card text-bg-light position-relative mx-4">
@@ -25,7 +29,7 @@ function printCards(par1, par2){
           <img src="./assets_day1/img/pin.svg" alt="pin">
         </div>
         <div class="card-img d-flex flex-column justify-content-center align-items-center">
-          <button><img src="${par1}" alt="placeholder img"></button>
+          <img draggable="false" class="clickable-img" src="${par1}" alt="placeholder img">
         </div>
         <div class="card-text">
           <p>${par2}</p>
@@ -40,3 +44,15 @@ function capitalizeWords(str) {
       .join(' ');
 };
 
+
+function printImg(src){
+overlayImg.innerHTML = `<img draggable="false" id="overlay-img" src="${src}" alt="test">`
+}
+
+function hide(element) {
+  element.classList.add('d-none')
+}
+
+function show(element){
+  element.classList.remove('d-none')
+}
